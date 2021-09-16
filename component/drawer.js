d7e20fbc67e17b022dc3271720aaa24619e2bbc1
DilankaPerera <dilankaperera27@gmail.com>
2021-09-16 23:09:45 +0530

    drawer updated

--- component/drawer.js
+++ component/drawer.js
@@ -2,19 +2,19 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-//import MyAds from './myads';
-//import Mechanics from './mechaniclist';
-//import Contact from './contactus';
+import MyAds from './myads';
+import Mechanics from './mechaniclist';
+import Contact from './contactus';
 import About from './aboutus';
-import Chat from './chat';
-//import Land from './landing';
 
 const Drawer = createDrawerNavigator();
 
 function MyDrawer() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Chat" component={Chat} />
+      <Drawer.Screen name="My Ads" component={MyAds} />
+      <Drawer.Screen name="Mechanics" component={Mechanics} />
+      <Drawer.Screen name="Contact Us" component={Contact} />
       <Drawer.Screen name="About" component={About} />
     </Drawer.Navigator>
   );
